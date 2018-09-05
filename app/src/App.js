c67a8ad0513d17972d011ddacf484e01dd4cd9b9
Brainguzzler <43007137+Brainguzzler@users.noreply.github.com>
2018-09-05 18:37:08 +0300

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,1 +1,38 @@
+import React from 'react';
+import { connect } from 'react-redux'
 
+import { getState } from './store/actions/app'
+
+import Header from './components/header'
+import Router from './Router'
+
+
+class App extends React.Component {
+
+  constructor(){
+    super()
+    this.startStateUpdateCycle();
+  }
+
+  startStateUpdateCycle(){
+    window.setInterval(() => this.props.getState(), 500);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header/>
+        <Router/>
+      </div>
+    );
+  }
+  
+}
+
+const mapStateToProps = (store) => {
+  return{}
+}
+
+export default connect(mapStateToProps,{
+  getState
+})(App);
